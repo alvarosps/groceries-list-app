3036a7d0039d95e8d49abb00f16d15abc6e9781c
Alvaro Silva <alvaro123sps@gmail.com>
2022-02-24 19:07:03 -0300

    Added dockerfiles

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,13 @@ import mongoose from "mongoose"
 import cors from "cors"
 import groceriesRoutes from "./routes"
 
+import path from "path"
+
 const app: Express = express()
+
+//This will create a middleware.
+//When you navigate to the root page, it would use the built react-app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 const PORT: string | number = process.env.PORT || 4000
 
